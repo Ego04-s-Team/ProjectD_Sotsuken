f4abc844f59ec8b24e685bc6ad754a33a8bfed73
221108 <221108@WINDOWS-DCFM6EC.mr.hi-joho.ac.jp>
2025-10-28 14:50:42 +0900

    2025/10/28
    Okada

---
+++ src/main/java/com/ProjectD/demo/SpringBootTestProject1Application.java
@@ -0,0 +1,13 @@
+package com.ProjectD.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTestProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTestProject1Application.class, args);
+	}
+
+}
