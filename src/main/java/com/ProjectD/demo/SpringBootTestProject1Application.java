6b22852e2c34f60f41d48caa8fdd4ac7428d0074
221108 <221108@WINDOWS-DCFM6EC.mr.hi-joho.ac.jp>
2025-10-31 15:34:00 +0900

    2025/10/31
    岡田
    IndexServlet.javaを追加＆実行ファイルでServletが動作するように改変

--- src/main/java/com/ProjectD/demo/SpringBootTestProject1Application.java
+++ src/main/java/com/ProjectD/demo/SpringBootTestProject1Application.java
@@ -3,8 +3,10 @@ package com.ProjectD.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@ServletComponentScan
 public class SpringBootTestProject1Application {
 
 	public static void main(String[] args) {
